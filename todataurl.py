1e49cbc38185690f1d2cdf047c0d02196ac7057f
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2012-03-16 16:06:54 -0400

    Data URL page generation.

--- todataurl.py
+++ todataurl.py
@@ -5,8 +5,31 @@ def convert(mime, data):
   return url
 
 jqueryurl = convert('text/javascript', file('jquery-1.3.2.min.js').read())
-html = file('set.html').read()
-html = html.replace('jquery-1.3.2.min.js', jqueryurl)
-url = convert('text/html', html)
+lines = []
+writing = True
+for line in file('set.html'):
+  if 'END JQUERY' in line:
+    writing = True
+    lines.append(
+      '<script type="text/javascript" src="'
+      + jqueryurl
+      + '"></script>\n')
 
+  if writing:
+    lines.append(line)
+
+  if 'BEGIN JQUERY' in line:
+    writing = False
+
+html = ''.join(lines)
+file('seturl.intermediate.html', 'w').write(html)
+
+url = convert('text/html', html)
 file('seturl.txt', 'w').write(url)
+
+page_lines = []
+page_lines.append('<h1>Offline Set</h1>\n')
+page_lines.append('<a href="' + url + '\n')
+page_lines.append('">Bookmark this link to play offline</a>\n')
+page_html = ''.join(page_lines)
+file('set_offline.html', 'w').write(page_html)
